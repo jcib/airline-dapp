94c1eceafa42a1176c1a1c3f8e43ff8e78fa3d35
J. Carlos <50453088+jcib@users.noreply.github.com>
2019-06-06 12:12:36 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
 import React, { Component } from "react";
 import Panel from "./Panel";
 
+import getWeb3 from "./getWeb3";
+
 export class App extends Component {
 
     constructor(props) {
         super(props);
+        this.state = { //estado inicial de la cuenta 0
+            account: undefined
+        };
     }
+
+    async componentDidMount(){
+
+        this.web3 = await getWeb3();
+        
+        var account = (await this.web3.eth.getAccounts())[0];
+        
+        this.setState ({ //con callback, esperar a que cargue
+            account: account.toLowerCase()
+        }, () => {
+            this.load();
+        });
+    }
+
+    
 
     render() {
         return <React.Fragment>
